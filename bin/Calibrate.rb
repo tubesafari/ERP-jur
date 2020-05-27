18c59620082775a2406a95fe9da2d8fd2cdfd903
tubesafari <119950956+tubesafari@users.noreply.github.com>
2020-05-27 21:51:29 +0000

    firs commit

--- bin/Calibrate.rb
+++ bin/Calibrate.rb
@@ -28,4 +28,14 @@ module MusicMaster
     # Parameters::
     # * *iConf* (<em>map<Symbol,Object></em>): The configuration
     # Return::
-    # * _Exception_
+    # * _Exception_: Error, or nil in case of success
+    def checkConf(iConf)
+      return nil
+    end
+
+    # Initialize Rake processes and return the task to be built
+    #
+    # Return::
+    # * _Symbol_: Rake target to execute
+    def getRakeTarget
+      initialize_Rak
