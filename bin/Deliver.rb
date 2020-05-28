888a3ef8dde9c39cd02aab63bf8a044ad595f197
tubesafari <119950956+tubesafari@users.noreply.github.com>
2020-05-28 15:03:15 +0000

    Test Commit

--- bin/Deliver.rb
+++ bin/Deliver.rb
@@ -1,1 +1,16 @@
-#!env ru
+#!env ruby
+
+require 'MusicMaster/Launcher'
+
+module MusicMaster
+
+  class Deliver < Launcher
+
+    protected
+
+    # Give additional command line options banner
+    #
+    # Return::
+    # * _String_: Options banner
+    def getOptionsBanner
+      return '[--name <Deli
