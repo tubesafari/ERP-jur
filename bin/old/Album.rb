1f424b7acb55fd976eec8238da97cf61b240884b
tubesafari <119950956+tubesafari@users.noreply.github.com>
2020-06-07 12:20:01 +0000

    Update sw.js

--- bin/old/Album.rb
+++ bin/old/Album.rb
@@ -1,4 +1,15 @@
 #!env ruby
 #--
 # Copyright (c) 2009 - 2012 Muriel Salvan (muriel@x-aeon.com)
-# Licensed under the terms specified in LICENSE file. 
+# Licensed under the terms specified in LICENSE file. No warranty is provided.
+#++
+
+require 'fileutils'
+require 'MusicMaster/Common'
+require 'rUtilAnts/Logging'
+RUtilAnts::Logging::install_logger_on_object
+require 'MusicMaster/ConfLoader'
+
+module MusicMaster
+
+  # Execute the a
