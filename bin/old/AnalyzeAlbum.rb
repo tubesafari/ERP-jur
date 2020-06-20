0ac01fc83f7515edbbc011bf5ec654eb3b63cc8c
tubesafari <119950956+tubesafari@users.noreply.github.com>
2020-06-20 13:47:11 +0000

    114.33+05

--- bin/old/AnalyzeAlbum.rb
+++ bin/old/AnalyzeAlbum.rb
@@ -1,3 +1,10 @@
 #!env ruby
 #--
-# Copyright (c) 2009
+# Copyright (c) 2009 - 2012 Muriel Salvan (muriel@x-aeon.com)
+# Licensed under the terms specified in LICENSE file. No warranty is provided.
+#++
+
+require 'fileutils'
+require 'MusicMaster/Common'
+require 'rUtilAnts/Logging'
+RUtilAnts::Logging::ins
