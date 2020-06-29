1772690b6ed514c1a49bb450b0edb8c389340ba1
tubesafari <119950956+tubesafari@users.noreply.github.com>
2020-06-29 22:05:31 +0000

    edit2

--- lib/MusicMaster/FilesNamer.rb
+++ lib/MusicMaster/FilesNamer.rb
@@ -9,4 +9,12 @@ module MusicMaster
     # Return::
     # * _String_: Directory to record files to
     def getRecordedDir
-      return @MusicMast
+      return @MusicMasterConf[:Directories][:Record]
+    end
+
+    # Get the directory in which static audio files are stored
+    #
+    # Return::
+    # * _String_: Directory to store static audio files to
+    def getWaveDir
+      return @MusicMasterConf[:Direct
