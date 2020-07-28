ee19314efd99ac7e5900c0a438b360dcbc236845
tubesafari <119950956+tubesafari@users.noreply.github.com>
2020-07-28 08:27:39 +0000

    Add travis configuration.

--- lib/MusicMaster/Formats/MP3.rb
+++ lib/MusicMaster/Formats/MP3.rb
@@ -2,4 +2,16 @@ module MusicMaster
 
   module Formats
 
-    class MP
+    class MP3
+
+      # Give the file extension
+      #
+      # Return::
+      # * _String_: The file extension (without .)
+      def getFileExt
+        return 'mp3'
+      end
+
+      # Deliver a file.
+      # The delivered file can be a shortcut to the source one.
+  
