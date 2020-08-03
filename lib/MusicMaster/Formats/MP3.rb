d5e33cea48f097ddf96ec2db6f4c93ae6046a4a0
tubesafari <119950956+tubesafari@users.noreply.github.com>
2020-08-03 18:11:04 +0000

    Add languages

--- lib/MusicMaster/Formats/MP3.rb
+++ lib/MusicMaster/Formats/MP3.rb
@@ -19,4 +19,6 @@ module MusicMaster
       # * *iSrcFileName* (_String_): The source file to deliver from
       # * *iDstFileName* (_String_): The destination file to be delivered
       # * *iFormatConf* (<em>map<Symbol,Object></em>): The format configuration
-  
+      # * *iMetadata* (<em>map<Symbol,Object></em>): The metadata that can be used while delivering the file
+      def deliver(iSrcFileName, iDstFileName, iFormatConf, iMetadata)
+        # TODO: Implement it using an external tool, and make regression tes
