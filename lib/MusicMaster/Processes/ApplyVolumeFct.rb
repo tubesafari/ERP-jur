76f839b68800ab72a92efe04c561cd0432d0518c
tubesafari <119950956+tubesafari@users.noreply.github.com>
2020-08-09 15:38:26 +0000

    i7core_edac: Fix PCI device reference count

--- lib/MusicMaster/Processes/ApplyVolumeFct.rb
+++ lib/MusicMaster/Processes/ApplyVolumeFct.rb
@@ -1,3 +1,11 @@
 require 'WSK/Common'
 
-mod
+module MusicMaster
+
+  module Processes
+
+    class ApplyVolumeFct
+
+      # Parameters of this process:
+      # * *:Function* (<em>map<Symbol,Object></em>): The function definition
+      # * *:Begin* (_String_): Position to app
