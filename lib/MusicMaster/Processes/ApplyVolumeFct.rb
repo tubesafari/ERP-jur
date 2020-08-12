43494e235ffa201aefe63a3d5694e14b6a41abab
tubesafari <119950956+tubesafari@users.noreply.github.com>
2020-08-12 14:00:46 +0000

    Bump snapshot (#5441)

--- lib/MusicMaster/Processes/ApplyVolumeFct.rb
+++ lib/MusicMaster/Processes/ApplyVolumeFct.rb
@@ -8,4 +8,5 @@ module MusicMaster
 
       # Parameters of this process:
       # * *:Function* (<em>map<Symbol,Object></em>): The function definition
-      # * *:Begin* (_String_): Position to app
+      # * *:Begin* (_String_): Position to apply volume transformation from. Can be specified as a sample number or a float seconds (ie. 12.3s).
+      # * *:End* (_String_): Position to apply volume transformation to. Can be specified as a sample number or a float seconds (ie. 12.3s). -1 means to the
