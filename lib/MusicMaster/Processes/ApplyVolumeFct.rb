32220a44826a9297c2d975d0677048e43cae05e0
tubesafari <119950956+tubesafari@users.noreply.github.com>
2020-08-12 22:01:20 +0000

    cast

--- lib/MusicMaster/Processes/ApplyVolumeFct.rb
+++ lib/MusicMaster/Processes/ApplyVolumeFct.rb
@@ -9,4 +9,12 @@ module MusicMaster
       # Parameters of this process:
       # * *:Function* (<em>map<Symbol,Object></em>): The function definition
       # * *:Begin* (_String_): Position to apply volume transformation from. Can be specified as a sample number or a float seconds (ie. 12.3s).
-      # * *:End* (_String_): Position to apply volume transformation to. Can be specified as a sample number or a float seconds (ie. 12.3s). -1 means to the
+      # * *:End* (_String_): Position to apply volume transformation to. Can be specified as a sample number or a float seconds (ie. 12.3s). -1 means to the end of file.
+      # * *:DBUnits* (_Boolean_): Are the units of the function in DB scale ? (else they are in a ratio scale).
+
+      include WSK::Common
+
+      # Execute the process
+      #
+      # Parameters::
+      # * *iInputFileName* (_Strin
