bc4731cdbcd19fe502aea5f60e32549f22074f35
tubesafari <119950956+tubesafari@users.noreply.github.com>
2020-08-14 08:56:18 +0000

    Version 1.1r24

--- lib/MusicMaster/Processes/ApplyVolumeFct.rb
+++ lib/MusicMaster/Processes/ApplyVolumeFct.rb
@@ -17,4 +17,8 @@ module MusicMaster
       # Execute the process
       #
       # Parameters::
-      # * *iInputFileName* (_Strin
+      # * *iInputFileName* (_String_): File name we want to apply effects to
+      # * *iOutputFileName* (_String_): File name to write
+      # * *iTempDir* (_String_): Temporary directory that can be used
+      # * *iParams* (<em>map<Symbol,Object></em>): Parameters
+      def execut
