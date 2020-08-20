826820926e98fb8232b312a489977df4f70823b1
tubesafari <119950956+tubesafari@users.noreply.github.com>
2020-08-20 13:41:48 +0000

    V2

--- lib/MusicMaster/Processes/DCShifter.rb
+++ lib/MusicMaster/Processes/DCShifter.rb
@@ -2,3 +2,25 @@
 module MusicMaster
 
   module Processes
+
+    class DCShifter
+
+      # Parameters of this process:
+      # * *:Offset* (_Integer_): The DC offset to apply (can be negative). The value's effect depends on the bit depth. A value of 64 in a 8 bits file will shift 50%. A value of 64 in a 16 bits file will shift 0.2%.
+
+      # Execute the process
+      #
+      # Parameters::
+      # * *iInputFileName* (_String_): File name we want to apply effects to
+      # * *iOutputFileName* (_String_): File name to write
+      # * *iTempDir* (_String_): Temporary directory that can be used
+      # * *iParams* (<em>map<Symbol,Object></em>): Parameters
+      def execute(iInputFileName, iOutputFileName, iTempDir, iParams)
+        wsk(iInputFileName, iOutputFileName, 'DCShifter', "--offset \"#{iParams[:Offset]}\"")
+      end
+
+    end
+
+  end
+
+end
---
+++ lib/MusicMaster/Processes/GVerb.rb
@@ -0,0 +1,12 @@
+module MusicMaster
+
+  module Processes
+
+    class GVerb
+
+      # Execute the process
+      #
+      # Parameters::
+      # * *iInputFileName* (_String_): File name we want to apply effects to
+      # * *iOutputFileName* (_String_): File name to write
+     
