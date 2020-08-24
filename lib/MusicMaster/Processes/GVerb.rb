f86437e697f8164631ba177d2c49a8d538a83397
tubesafari <119950956+tubesafari@users.noreply.github.com>
2020-08-24 08:43:00 +0000

    merged development

--- lib/MusicMaster/Processes/GVerb.rb
+++ lib/MusicMaster/Processes/GVerb.rb
@@ -9,4 +9,7 @@ module MusicMaster
       # Parameters::
       # * *iInputFileName* (_String_): File name we want to apply effects to
       # * *iOutputFileName* (_String_): File name to write
-     
+      # * *iTempDir* (_String_): Temporary directory that can be used
+      # * *iParams* (<em>map<Symbol,Object></em>): Parameters
+      def execute(iInputFileName, iOutputFileName, iTempDir, iParams)
+        # TODO: Use a real GVerb library, and implement regre
