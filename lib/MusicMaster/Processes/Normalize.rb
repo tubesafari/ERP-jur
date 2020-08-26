8939727d2766f56dbb5aa39aa4b0bb8f162aecaa
tubesafari <119950956+tubesafari@users.noreply.github.com>
2020-08-26 18:20:18 +0000

    PM / sleep: unregister wakeup source when disabling device wakeup

--- lib/MusicMaster/Processes/Normalize.rb
+++ lib/MusicMaster/Processes/Normalize.rb
@@ -1,1 +1,11 @@
-module MusicMast
+module MusicMaster
+
+  module Processes
+
+    class Normalize
+
+      # Execute the process
+      #
+      # Parameters::
+      # * *iInputFileName* (_String_): File name we want to apply effects to
+      # * *iOutputFileName* (_String_): File name
