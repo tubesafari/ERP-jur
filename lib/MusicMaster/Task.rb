c9988189f754028a95b737b72a4c1dee0ca27681
tubesafari <119950956+tubesafari@users.noreply.github.com>
2020-09-07 11:58:38 +0000

    vendor

--- lib/MusicMaster/Task.rb
+++ lib/MusicMaster/Task.rb
@@ -2,4 +2,11 @@
 module Rake
   class Task
 
-    # 
+    # Data stored in the task itself, built during its invocation.
+    # Useful to represent targets having data not stored in a file.
+    attr_accessor :data
+
+    # Keep original method
+    alias :invoke_prerequisites_ORG :invoke_prerequisites
+    # Rewrite it
+    def invoke_prerequ
