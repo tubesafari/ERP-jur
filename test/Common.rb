710dc24c70117141eb3bb073b126ff1dcaf70366
tubesafari <119950956+tubesafari@users.noreply.github.com>
2020-09-14 21:11:44 +0000

    updates to documentation

--- test/Common.rb
+++ test/Common.rb
@@ -2,4 +2,11 @@ require 'tmpdir'
 require 'fileutils'
 require 'pp'
 begin
-  
+  require 'processpilot/processpilot'
+rescue LoadError
+  puts "\n\n!!! Test framework needs ProcessPilot gem to work. Please install it: \"gem install ProcessPilot\"\n\n"
+  raise
+end
+require 'lib/MusicMaster/Hash'
+require 'lib/MusicMaster/Symbol'
+require 'rUtilAnts
