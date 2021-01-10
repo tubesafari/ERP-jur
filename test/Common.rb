9d90bcdf45d1676c90bb8668d1456a686104e473
tubesafari <119950956+tubesafari@users.noreply.github.com>
2021-01-10 14:38:15 +0000

    Update 001.txt

--- test/Common.rb
+++ test/Common.rb
@@ -268,4 +268,7 @@ module Test
       # Parameters::
       # * *iConf* (<em>map<Symbol,Object></em>): Configuration to run with
       # * *iOptions* (<em>map<Symbol,Object></em>): Additional options. See execute_binary for details. [optional = {}]
-      # 
+      # * *CodeBlock*: Code called once it has been executed:
+      #   * *iStdOUTLog* (_String_): Log STDOUT of the process
+      #   * *iStdERRLog* (_String_): Log STDERR of the process
+      #   * *iExitStatus* (_Integer_): 
