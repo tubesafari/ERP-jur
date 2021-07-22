0a39aee784af3c3f8c56879e33246c37e4f086b4
tubesafari <119950956+tubesafari@users.noreply.github.com>
2021-07-22 09:07:48 +0000

    Updated pom

--- test/Common.rb
+++ test/Common.rb
@@ -401,4 +401,8 @@ module Test
               [ 'Wave/02_Clean/Record/Calibration.Env1.Env4.04.NoiseGate.wav', '02_Clean/Record/Calibration.Env1.Env4.04.NoiseGate.wav' ],
               [ 'Wave/02_Clean/Record/Calibration.Env4.Env1.01.SilenceRemover.wav', '02_Clean/Record/Calibration.Env4.Env1.01.SilenceRemover.wav' ],
               [ 'Wave/02_Clean/Record/Calibration.Env4.Env1.02.Cut.0.01s_0.16s.wav', '02_Clean/Record/Calibration.Env4.Env1.02.Cut.0.01s_0.16s.wav' ],
-              [ 'Wave/02_Clean/Record/Calibration.Env4.Env1.03.DCShifter.wav', '02_Clean/Record/Calibration.Env4.E
+              [ 'Wave/02_Clean/Record/Calibration.Env4.Env1.03.DCShifter.wav', '02_Clean/Record/Calibration.Env4.Env1.03.DCShifter.wav' ],
+              [ 'Wave/02_Clean/Record/Calibration.Env4.Env1.04.NoiseGate.wav', '02_Clean/Record/Calibration.Env4.Env1.04.NoiseGate.wav' ]
+            ])
+          when :Calibrated_Env1_1_CalibEnv4
+            rLstFiles.conca
