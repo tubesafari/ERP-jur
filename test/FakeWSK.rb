087c68001c2daa8c91de8e36d743beef4923b6c5
tubesafari <119950956+tubesafari@users.noreply.github.com>
2021-08-29 16:55:58 +0000

    Unit test fixes.

--- test/FakeWSK.rb
+++ test/FakeWSK.rb
@@ -1,1 +1,9 @@
-p
+puts "Fake WSK invoked: #{ARGV.inspect}"
+
+def fail(iStr)
+  puts "!!! Fake WSK error: #{iStr}"
+  raise iStr
+end
+
+def format(iInfo)
+  rStr = ":Input => '#{iInfo[:Input]}',\n:Output => '#{iInfo[:Output]}',\n:Action => '#
