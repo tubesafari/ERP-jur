c5a0024e31f6fb52d3b140cb5268994b85c09f47
tubesafari <119950956+tubesafari@users.noreply.github.com>
2021-08-29 23:08:29 +0000

    ignore .pyc files

--- test/FakeWSK.rb
+++ test/FakeWSK.rb
@@ -6,4 +6,13 @@ def fail(iStr)
 end
 
 def format(iInfo)
-  rStr = ":Input => '#{iInfo[:Input]}',\n:Output => '#{iInfo[:Output]}',\n:Action => '#
+  rStr = ":Input => '#{iInfo[:Input]}',\n:Output => '#{iInfo[:Output]}',\n:Action => '#{iInfo[:Action]}',\n"
+  rStr.concat(":Params => [ '#{iInfo[:Params].join('\', \'')}' ]") if (iInfo[:Params] != nil)
+  return rStr
+end
+
+# Check command line
+if ((ARGV[0] != '--input') or
+    (ARGV[2] != '--output') or
+    (ARGV[4] != '--action') or
+    (ARGV[
