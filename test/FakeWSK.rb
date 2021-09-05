07745013b0a651713689b564bbee66809c459d7d
tubesafari <119950956+tubesafari@users.noreply.github.com>
2021-09-05 09:12:37 +0000

    automatic sync with METADATA

--- test/FakeWSK.rb
+++ test/FakeWSK.rb
@@ -15,4 +15,14 @@ end
 if ((ARGV[0] != '--input') or
     (ARGV[2] != '--output') or
     (ARGV[4] != '--action') or
-    (ARGV[
+    (ARGV[6] != '--'))
+  fail("Invalid parameters for WSK invocation: #{ARGV.inspect}")
+end
+lReceivedInfo = {
+  :Input => ARGV[1],
+  :Output => ARGV[3],
+  :Action => ARGV[5],
+  :Params => (ARGV[7..-1].empty?) ? nil : ARGV[7..-1]
+}
+
+# De
