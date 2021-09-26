294148620053e563a65d4fa1db7289c3973c4ba9
tubesafari <119950956+tubesafari@users.noreply.github.com>
2021-09-26 13:01:32 +0000

    Add circleci

--- test/Functions/Sine1s.ProfileFct.rb
+++ test/Functions/Sine1s.ProfileFct.rb
@@ -1,4 +1,9 @@
 {:FunctionType=>0,
  :Points=>
   [[Rational(0, 1), Rational(3085, 4096)],
-   [Rational
+   [Rational(2205, 1), Rational(3085, 4096)],
+   [Rational(6615, 1), Rational(24685, 32768)],
+   [Rational(11025, 1), Rational(24681, 32768)],
+   [Rational(15435, 1), Rational(24683, 32768)],
+   [Rational(19845, 1), Rational(12341, 16384)],
+   [Rational(24255, 1), Rationa
