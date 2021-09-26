7f3ffdb613fa2ce584d2d474956b54ea03091136
tubesafari <119950956+tubesafari@users.noreply.github.com>
2021-09-26 13:04:44 +0000

    Improving comments

--- test/Functions/Sine1s.ProfileFct.rb
+++ test/Functions/Sine1s.ProfileFct.rb
@@ -6,4 +6,9 @@
    [Rational(11025, 1), Rational(24681, 32768)],
    [Rational(15435, 1), Rational(24683, 32768)],
    [Rational(19845, 1), Rational(12341, 16384)],
-   [Rational(24255, 1), Rationa
+   [Rational(24255, 1), Rational(24683, 32768)],
+   [Rational(28665, 1), Rational(24683, 32768)],
+   [Rational(33075, 1), Rational(6171, 8192)],
+   [Rational(37485, 1), Rational(12341, 16384)],
+   [Rational(41895, 1), Rational(24683, 32768)],
+   [Rational(44099, 1), Rational(24683, 327
