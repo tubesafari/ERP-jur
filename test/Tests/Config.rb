d9fc68c927988b5363b86024d186928175354ee0
tubesafari <119950956+tubesafari@users.noreply.github.com>
2021-10-03 05:31:26 +0000

    Bumped build

--- test/Tests/Config.rb
+++ test/Tests/Config.rb
@@ -2,4 +2,12 @@ module MusicMasterTest
 
   class Config < ::Test::Unit::TestCase
 
-    # 
+    # Missing config file
+    def testMissingConfigFile
+      execute_Record([]) do |iStdOUTLog, iStdERRLog, iExitStatus|
+        assert_exitstatus 1, iExitStatus
+        assert_match 'Please specify 1 config file', iStdERRLog
+      end
+    end
+
+    # E
