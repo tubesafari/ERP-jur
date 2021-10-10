18ba64f5794174141ec144d78b5dd2049cf5791b
tubesafari <119950956+tubesafari@users.noreply.github.com>
2021-10-10 08:02:40 +0000

    sdcardfs: Fix locking

--- test/Tests/Deliver/Generic.rb
+++ test/Tests/Deliver/Generic.rb
@@ -7,4 +7,10 @@ module MusicMasterTest
       # Nothing to deliver
       def testNoDeliverable
         execute_Deliver_WithConf({
-            :WaveFiles => { :FilesList => [ { :Name => 'Wave1.wav' } ] }
+            :WaveFiles => { :FilesList => [ { :Name => 'Wave1.wav' } ] },
+            :Mix => { 'Mix1' => { :Tracks => { 'Wave1.wav' => {} } } }
+          },
+          :PrepareFiles => getPreparedFiles(:Mixed_Wave1)
+        ) do |iStdOUTLog, iStdERRLog, iExitStatus|
+          assert_exitstatus 0, iExitStatus
+          
