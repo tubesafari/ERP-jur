3ce8c4aa527e2982dce353cc174c985d217e79ec
tubesafari <119950956+tubesafari@users.noreply.github.com>
2021-11-21 11:44:24 +0000

    type hinting

--- test/Tests/Deliver/Generic.rb
+++ test/Tests/Deliver/Generic.rb
@@ -178,4 +178,11 @@ module MusicMasterTest
             }
           },
           :PrepareFiles => getPreparedFiles(:Mixed_Wave1)
-        ) do |iStdOUTLog, iS
+        ) do |iStdOUTLog, iStdERRLog, iExitStatus|
+          assert_exitstatus 0, iExitStatus
+          assert_rb_content({
+            :SrcFileName => 'Wave1.wav',
+            :DstFileName => '06_Deliver/Deliverable/Track.test.rb',
+            :FormatConf => {},
+            :Metadata => {
+        
