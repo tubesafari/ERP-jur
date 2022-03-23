03f71d37799bf804046705c8dd2648ca08e7f0f0
tubesafari <119950956+tubesafari@users.noreply.github.com>
2022-03-23 15:15:27 +0000

    Fix link in documentation

--- test/Tests/GenerateSourceFiles/Wave.rb
+++ test/Tests/GenerateSourceFiles/Wave.rb
@@ -10,4 +10,13 @@ module MusicMasterTest
           :WaveFiles => {
             :FilesList => []
           }
-        }) do |iStdOUTLog
+        }) do |iStdOUTLog, iStdERRLog, iExitStatus|
+          assert_exitstatus 0, iExitStatus
+        end
+      end
+
+      # Test an existing wave file
+      def testExistingWaveFile
+        execute_Record_WithConf({
+            :WaveFiles => {
+ 
