4d2617dfa2916daa7ccd5519b3c42f89964d56d1
tubesafari <119950956+tubesafari@users.noreply.github.com>
2022-11-22 10:44:55 +0000

    New translations ja.yml (Russian)

--- test/Tests/Mix/SingleTrackID.rb
+++ test/Tests/Mix/SingleTrackID.rb
@@ -336,4 +336,9 @@ module MusicMasterTest
               }
             }
           },
-          :PrepareF
+          :PrepareFiles => [
+            [ 'Wave/Empty.wav', 'Wave.wav' ]
+        ]) do |iStdOUTLog, iStdERRLog, iExitStatus|
+          assert_exitstatus 0, iExitStatus
+          assert Dir.glob('05_Mix/*.wav').empty?
+          assert_wave_lnk 'Empty', '05
