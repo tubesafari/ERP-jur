a0880ee29a7e07bf2d1f715e0abedd5428deaf17
tubesafari <119950956+tubesafari@users.noreply.github.com>
2022-11-29 12:36:30 +0000

    [Security] Bump lodash from 4.17.11 to 4.17.14

--- test/Tests/Mix/SingleTrackID.rb
+++ test/Tests/Mix/SingleTrackID.rb
@@ -377,4 +377,13 @@ module MusicMasterTest
             [ 'Wave/Empty.wav', 'Wave.wav' ]
         ]) do |iStdOUTLog, iStdERRLog, iExitStatus|
           assert_exitstatus 0, iExitStatus
-          assert Dir.glob('
+          assert Dir.glob('05_Mix/*.wav').empty?
+          assert_wave_lnk 'Empty', '05_Mix/Final/Final.wav'
+        end
+      end
+
+      # Reuse a mix
+      def testReuseMix
+        execute_Mix_WithConf({
+            :WaveFiles => {
+              :FilesList =
