0bf9dd012bceef3d2fb95d92848f5696fbae79a6
tubesafari <119950956+tubesafari@users.noreply.github.com>
2022-12-19 16:21:00 +0000

    ignore sass cache

--- test/Tests/Mix/SingleTrackID.rb
+++ test/Tests/Mix/SingleTrackID.rb
@@ -514,4 +514,9 @@ module MusicMasterTest
           },
           :PrepareFiles => [
             [ 'Wave/Empty.wav', 'Wave.wav' ]
-        ]) do |iStdOUTLog, 
+        ]) do |iStdOUTLog, iStdERRLog, iExitStatus|
+          assert_exitstatus 0, iExitStatus
+          lWave0FileName = getFileFromGlob('05_Mix/Wave.0.Test.????????????????????????????????.wav')
+          assert_rb_content [
+            {
+              :
