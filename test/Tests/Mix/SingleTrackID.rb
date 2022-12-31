2d8d76bb82a86d9b504ba29fa117c8750cc985c0
tubesafari <119950956+tubesafari@users.noreply.github.com>
2022-12-31 21:16:55 +0000

    added helpers

--- test/Tests/Mix/SingleTrackID.rb
+++ test/Tests/Mix/SingleTrackID.rb
@@ -634,4 +634,8 @@ module MusicMasterTest
             [ 'Wave/Empty.wav', 'Wave.wav' ],
             [ 'Wave/Noise1s.wav', "05_Mix/Wave.0.Test.#{lProcessID}.wav" ]
         ]) do |iStdOUTLog, iStdERRLog, iExitStatus|
-    
+          assert_exitstatus 0, iExitStatus
+          lWave0FileName = getFileFromGlob('05_Mix/Wave.0.Test.0.Test.????????????????????????????????.wav')
+          assert_rb_content [
+            {
+              :InputFileName => "05_Mix/Wave.0.Test.#{lProcessID}.w
