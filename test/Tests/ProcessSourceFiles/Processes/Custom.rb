f7a8e7af8c1eb6553bfb0f66296049aa730dfcc7
tubesafari <119950956+tubesafari@users.noreply.github.com>
2023-02-21 10:03:04 +0000

    Update application.properties

--- test/Tests/ProcessSourceFiles/Processes/Custom.rb
+++ test/Tests/ProcessSourceFiles/Processes/Custom.rb
@@ -2,4 +2,13 @@ module MusicMasterTest
 
   module ProcessSourceFiles
 
-    
+    module Processes
+
+      class Custom < ::Test::Unit::TestCase
+
+        # Normal invocation
+        def testNormalUsage
+          lProcessID = {
+            :CustomParam1 => 'Param1Value'
+          }.unique_id
+          execute_Process_Wit
