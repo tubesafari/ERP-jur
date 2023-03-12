b9342cc8cdc9228b0c8f24c4172626299eb9fb28
tubesafari <119950956+tubesafari@users.noreply.github.com>
2023-03-12 18:32:33 +0000

    Updates Documents/db_mysql.md

--- test/Tests/ProcessSourceFiles/Processes/Cut.rb
+++ test/Tests/ProcessSourceFiles/Processes/Cut.rb
@@ -22,4 +22,12 @@ module MusicMasterTest
                     ]
                   }
                 ]
-          
+              }
+            },
+            :PrepareFiles => [
+              [ 'Wave/Empty.wav', 'Wave.wav' ]
+            ],
+            :FakeWSK => [
+              {
+                :Input => 'Wave.wav',
+                :Output => /04_Process\/Wave\/Wave\.0\.Cut\.[[:xdigit:]]{32,3
