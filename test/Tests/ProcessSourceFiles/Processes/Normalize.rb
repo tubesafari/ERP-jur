ccac297c37e3828df4fc4a833fe7aa77ff385750
tubesafari <119950956+tubesafari@users.noreply.github.com>
2023-03-24 06:28:35 +0000

    TimeTypes

--- test/Tests/ProcessSourceFiles/Processes/Normalize.rb
+++ test/Tests/ProcessSourceFiles/Processes/Normalize.rb
@@ -64,3 +64,35 @@ module MusicMasterTest
                 ]
               }
             },
+            :PrepareFiles => [
+              [ 'Wave/Peak1s.wav', 'Wave.wav' ]
+            ],
+            :FakeWSK => [
+              {
+                :Input => 'Wave.wav',
+                :Output => './Dummy.wav',
+                :Action => 'Analyze',
+                :UseWave => 'Empty.wav',
+                :CopyFiles => { 'Analysis/Peak1s.analyze' => 'analyze.result' }
+              },
+              {
+                :Input => 'Wave.wav',
+                :Output => /04_Process\/Wave\/Wave\.0\.Normalize\.[[:xdigit:]]{32,32}\.wav/,
+                :Action => 'Multiply',
+                :Params => [ '--coeff', '1/1' ],
+                :UseWave => 'Empty.wav',
+              }
+          ]) do |iStdOUTLog, iStdERRLog, iExitStatus|
+            assert_exitstatus 0, iExitStatus
+            getFileFromGlob('04_Process/Wave/Wave.0.Normalize.????????????????????????????????.wav')
+            assert File.exists?('Wave.wav.analyze')
+          end
+        end
+
+      end
+
+    end
+
+  end
+
+end
---
+++ test/Tests/ProcessSourceFiles/Processes/SilenceInserter.rb
@@ -0,0 +1,11 @@
+module MusicMasterTest
+
+  module ProcessSourceFiles
+
+    module Processes
+
+      class SilenceInserter < ::Test::Unit::TestCase
+
+        # Normal invocation
+        def testNormalUsage
+          execute_Proc
