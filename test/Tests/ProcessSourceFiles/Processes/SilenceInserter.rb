6534cfa47f14e3e78ca891e26f73564389444040
tubesafari <119950956+tubesafari@users.noreply.github.com>
2023-03-26 10:00:09 +0000

    Update itemsguideunified.html

--- test/Tests/ProcessSourceFiles/Processes/SilenceInserter.rb
+++ test/Tests/ProcessSourceFiles/Processes/SilenceInserter.rb
@@ -32,3 +32,9 @@ module MusicMasterTest
                 :Input => 'Wave.wav',
                 :Output => /04_Process\/Wave\/Wave\.0\.SilenceInserter\.[[:xdigit:]]{32,32}\.wav/,
                 :Action => 'SilenceInserter',
+                :Params => [ '--begin', '0.1s', '--end', '0.9s' ],
+                :UseWave => 'Empty.wav'
+              }
+          ]) do |iStdOUTLog, iStdERRLog, iExitStatus|
+            assert_exitstatus 0, iExitStatus
+            getFileFromGlob('04_Process/Wave/Wave.0.
